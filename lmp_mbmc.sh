7895064ee0db57d992d49bc9c03f25f59fb5ea08
Shuozhi Xu <shuozhixu@gmail.com>
2024-02-26 10:46:25 -0600

    Update lmp_mbmc.sh

--- lmp_mbmc.sh
+++ lmp_mbmc.sh
@@ -2,7 +2,7 @@
 
 rm -rf lammps-mbmc
 module load intelmpi/2021.3.0-intel2021.3.0 gcc/10.2.0 cuda/11.7.1
-git clone -b stable https://github.com/lammps/lammps.git lammps-mbecmc
+git clone -b stable https://github.com/lammps/lammps.git lammps-mbmc
 cd lammps-mbmc/src
 make yes-manybody
 make yes-mc
